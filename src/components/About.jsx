c5906aeb4d1ec7578aeb84fa4d601ce8dbb49a67
nabalmes <neilbalmes4200@gmail.com>
2024-08-22 22:18:06 +0800

    added: portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <Card>
       <h1>About this project</h1>
       <p>This is a React app to leave feedback for a product or service</p>
-      <p>Version  1.0.0</p>
+      <p>Version  1.0.0 / nab2023.vercel.app</p>
     </Card>
   )
 }
